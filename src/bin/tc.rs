eb70ce4ac487243d2fb13656d05fc134ecf8dd72
Paul Dettorer Hervot <dettorer@dettorer.net>
2022-06-20 14:12:20 +0200

    tc: run rustfmt

--- src/bin/tc.rs
+++ src/bin/tc.rs
@@ -4,5 +4,8 @@ use tc::parser::lexer::TokenStream;
 
 fn main() {
     let lexer = TokenStream::new(io::stdin().lock());
-    println!("There are {} tokens in the input", lexer.into_iter().count());
+    println!(
+        "There are {} tokens in the input",
+        lexer.into_iter().count()
+    );
 }
